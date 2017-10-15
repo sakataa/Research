68de7974954380e3d90d8a9f49fc833bc4619956
sakataa <tientm.ctt@gmail.com>
2017-10-15 22:09:18 +0700

    20171015 - Add NodeServices Demo

---
+++ NetCore/ASPNetCore/mvcappwithnode/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+using mvcappwithnode.Models;
+
+namespace mvcappwithnode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> NodeServices([FromServices] INodeServices nodeServices)
+        {
+            var options = new { width = 700, height = 500, showArea = true, showPoint = true, fullWidth = true };
+            var data = new
+            {
+                labels = new[] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" },
+                series = new[] {
+                    new[] { 1, 5, 2, 5, 4, 3 },
+                    new[] { 2, 3, 4, 8, 1, 2 },
+                    new[] { 5, 4, 3, 2, 1, 0 }
+                }
+            };
+
+            ViewData["ChartMarkup"] = await nodeServices.InvokeAsync<string>("wwwroot/js/site.js", "line", options, data);
+
+            return View();
+        }
+    }
+}
