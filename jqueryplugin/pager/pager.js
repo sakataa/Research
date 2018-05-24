859bbda2273a81790b8ada574dbb149d7702eabc
Walter Tran <walter.tran@starixsoft.com>
2018-05-24 19:27:25 +0700

    20180524 - Add pager jquery plugin

---
+++ jqueryplugin/pager/pager.js
@@ -0,0 +1,182 @@
+﻿(function ($) {
+    var DEFAULT_PAGESIZE_LIST = [10, 20, 50, 100, 200];
+    var defaultOption = {
+        pageIndex: 0,
+        pageSize: 20,
+        totalItems: 0,
+        pageSizeList: DEFAULT_PAGESIZE_LIST,
+        onChange: function (pageIndex, pageSize) { },
+        onClickPrev: function () { },
+        onClickLastPrev: function () { },
+        onClickNext: function () { },
+        onClickLastNext: function () { },
+        onChangePageSize: function () { },
+    };
+
+    function buildPrevButton() {
+        var html = ['<a href="javascript:;" title="First" class="icon glyphicon glyphicon-fast-backward page-first disable"></a>'];
+        html.push('<a href="javascript:;" title="Prev" class="icon glyphicon glyphicon-triangle-left page-prev disable"></a>');
+
+        return html.join('');
+    }
+
+    function buildNextButton() {
+        var html = ['<a href="javascript:;" title="Next" class="icon glyphicon glyphicon-triangle-right page-next"></a>'];
+        html.push('<a href="javascript:;" title="Last" class="icon glyphicon glyphicon-fast-forward page-last"></a>');
+
+        return html.join('');
+    }
+
+    function buildPageInfo(totalPage) {
+        var html = ['<div class="page-input">'];
+        html.push('<span class="text-pageInfo">Page 1 of ' + (totalPage || 1) + '</span>')
+        html.push('</div>');
+
+        return html.join('');
+    }
+
+    function buildPageSizeList(pageSizeList, selectedItem) {
+        var htmlPageSize = ['<select class="pagesize-select" >'];
+        for (var i = 0; i < pageSizeList.length; i++) {
+            var pageSize = pageSizeList[i];
+            var selected = pageSize === selectedItem ? "selected" : "";
+            htmlPageSize.push('<option value="' + pageSize + '" ' + selected + '>' + pageSize + '</option>');
+        }
+        htmlPageSize.push('</select>');
+
+        return htmlPageSize.join('');
+    }
+
+    function updatePageInfo($pagerContainer, pageIndex, totalPage) {
+        $pagerContainer.find('.text-pageInfo').text('Page ' + pageIndex + ' of ' + totalPage);
+    }
+
+    function togglePrevButton($pagerContainer, isEnable) {
+        $pagerContainer.find('.page-first').toggleClass('disable', !isEnable);
+        $pagerContainer.find('.page-prev').toggleClass('disable', !isEnable);
+    }
+
+    function toggleNextButton($pagerContainer, isEnable) {
+        $pagerContainer.find('.page-next').toggleClass('disable', !isEnable);
+        $pagerContainer.find('.page-last').toggleClass('disable', !isEnable);
+    }
+
+    var _methods = {
+        init: function (option) {
+            return this.each(function () {
+                var mainOption = $.extend(defaultOption, option || {});
+
+                var $self = $(this);
+                var totalPage = parseInt(mainOption.totalItems / mainOption.pageSize) + (mainOption.totalItems % mainOption.pageSize === 0 ? 0 : 1);
+                $self.pageInfo = {
+                    pageIndex: mainOption.pageIndex,
+                    pageSize: mainOption.pageSize,
+                    totalItems: mainOption.totalItems,
+                    totalPage: totalPage
+                }
+
+                var htmlPrevButton = buildPrevButton();
+                var htmlPagingInfo = buildPageInfo(totalPage);
+                var htmlNextButton = buildNextButton();
+                var htmlPageSizeList = buildPageSizeList(mainOption.pageSizeList, mainOption.pageSize);
+
+                var $pagerContainer = $('<div class="page-container"></div>');
+                $pagerContainer.append(htmlPrevButton + htmlPagingInfo + htmlNextButton + htmlPageSizeList);
+                $self.append($pagerContainer);
+
+                $pagerContainer.on('click', '.page-first', function () {
+                    var pageInfo = $self.pageInfo;
+                    if (pageInfo.pageIndex === 0) {
+                        return;
+                    }
+
+                    pageInfo.pageIndex = 0;
+                    updatePageInfo($pagerContainer, pageInfo.pageIndex + 1, pageInfo.totalPage);
+                    togglePrevButton($pagerContainer, false);
+                    toggleNextButton($pagerContainer, true);
+
+                    mainOption.onChange(pageInfo.pageIndex, pageInfo.pageSize);
+                });
+
+                $pagerContainer.on('click', '.page-prev', function () {
+                    var pageInfo = $self.pageInfo;
+                    if (pageInfo.pageIndex === 0) {
+                        return;
+                    }
+
+                    pageInfo.pageIndex--;
+                    if (pageInfo.pageIndex === 0) {
+                        togglePrevButton($pagerContainer, false);
+                    }
+
+                    toggleNextButton($pagerContainer, true);
+                    updatePageInfo($pagerContainer, pageInfo.pageIndex + 1, pageInfo.totalPage);
+
+                    mainOption.onChange(pageInfo.pageIndex, pageInfo.pageSize);
+                });
+
+                $pagerContainer.on('click', '.page-next', function () {
+                    var pageInfo = $self.pageInfo;
+                    console.log(pageInfo);
+                    if (pageInfo.pageIndex === pageInfo.totalPage - 1) {
+                        return;
+                    }
+
+                    pageInfo.pageIndex++;
+                    if (pageInfo.pageIndex === pageInfo.totalPage - 1) {
+                        toggleNextButton($pagerContainer, false);
+                    }
+
+                    togglePrevButton($pagerContainer, true);
+                    updatePageInfo($pagerContainer, pageInfo.pageIndex + 1, pageInfo.totalPage);
+
+                    mainOption.onChange(pageInfo.pageIndex, pageInfo.pageSize);
+                });
+
+                $pagerContainer.on('click', '.page-last', function () {
+                    var pageInfo = $self.pageInfo;
+                    if (pageInfo.pageIndex === pageInfo.totalPage - 1) {
+                        return;
+                    }
+
+                    pageInfo.pageIndex = pageInfo.totalPage - 1;
+                    updatePageInfo($pagerContainer, pageInfo.pageIndex + 1, pageInfo.totalPage);
+                    togglePrevButton($pagerContainer, true);
+                    toggleNextButton($pagerContainer, false);
+
+                    mainOption.onChange(pageInfo.pageIndex, pageInfo.pageSize);
+                });
+
+                $pagerContainer.on('change', '.pagesize-select', function () {
+                    var pageInfo = $self.pageInfo;
+                    pageInfo.pageIndex = 0;
+                    pageInfo.pageSize = parseInt($(this).val());
+
+                    var totalPage = parseInt(pageInfo.totalItems / pageInfo.pageSize) + (pageInfo.totalItems % pageInfo.pageSize === 0 ? 0 : 1);
+                    pageInfo.totalPage = totalPage;
+
+                    updatePageInfo($pagerContainer, pageInfo.pageIndex + 1, pageInfo.totalPage);
+                    togglePrevButton($pagerContainer, false);
+                    toggleNextButton($pagerContainer, true);
+
+                    mainOption.onChange(pageInfo.pageIndex, pageInfo.pageSize);
+                });
+            });
+        },
+
+        update: function (pageInfo) {
+            var $self = $(this);
+        }
+    }
+
+    $.fn.pager = function (methodOrOptions) {
+        if (_methods[methodOrOptions]) {
+            return _methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
+            // Default to "init"
+            return _methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + methodOrOptions + ' does not exist on jQuery.flatPopup');
+        }
+    };
+})(jQuery)
